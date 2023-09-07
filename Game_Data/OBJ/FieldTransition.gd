6866ad9cc05c47bf1853e5b9af75ff0df57e7a83
Teltt <129358512+Teltt@users.noreply.github.com>
2023-09-07 17:22:02 -0600

    update objects

--- Game_Data/OBJ/FieldTransition.gd
+++ Game_Data/OBJ/FieldTransition.gd
@@ -2,6 +2,7 @@ extends Interactable
 class_name FieldTransition
 
 @export var teleport_mc_to:Vector2
+@export var teleport_mc_to_node:String
 @export_file var field:String
 @export var change_time:bool = false
 @export var time = Game.TimeOfDay.Evening
@@ -16,5 +17,10 @@ func interact():
 		Game.main.change_time(time)
 	Game.main.load_new_field(load(field))
 	await(Game.main.load_fade.loaded)
-	Game.main.mc.position = teleport_mc_to
-	Game.main.camera.position = teleport_mc_to
+	if teleport_mc_to_node.is_empty() or not Game.main.field.has_node(teleport_mc_to_node):
+		Game.main.mc.position = teleport_mc_to
+		Game.main.camera.position = teleport_mc_to
+	else: 
+		var teleport = Game.main.field.get_node(teleport_mc_to_node).global_position
+		Game.main.mc.global_position = teleport
+		Game.main.camera.global_position = teleport
